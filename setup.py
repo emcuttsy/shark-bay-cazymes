77bf50f9ce5b9346b44cee2331beddc07a2275ea
Elise Cutts <elise.m.cutts@gmail.com>
2021-05-21 10:10:32 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of CAZyme distribution 4 metagenome-assembled genomes from a Shark Bay pustular mat',
+    author='Elise Cutts',
+    license='MIT',
+)
