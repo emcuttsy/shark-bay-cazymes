087b0dc2a7f5767250cbb0819ba6d80dc49b024c
Elise <elise.m.cutts@gmail.com>
2021-05-24 16:53:00 +0200

    ehlpers

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,7 @@ import logging
 from pathlib import Path
 #import pandas as pd
 import src.config as config
+import make_dataset_helpers as helpers
 import os
 #from dotenv import find_dotenv, load_dotenv
 
@@ -24,12 +25,12 @@ def main():
     overviews = {}
     for i in Path(config.data_dir / 'raw' / 'dbCAN').iterdir():
         try:
-            overview = get_mag_dbcan_overview(i)
+            overview = helpers.get_mag_dbcan_overview(i)
             print('success!')
         except:
             print(i)
 
-        overviews[i] = remove_HMMer_bounds(overview)
+        overviews[i] = helpers.remove_HMMer_bounds(overview)
         overviews[i] = overview[overview['#ofTools'] > 1] # remove entries with less than 2 tools reporting.
 
 if __name__ == '__main__':
@@ -44,63 +45,3 @@ if __name__ == '__main__':
     #load_dotenv(find_dotenv())
 
     main()
-
-######################################################################################
-# Functions used in data processing
-######################################################################################
-
-def get_mag_dbcan_overview(mag_dir):
-    # locate the overview file in the dbCAN results for a MAG
-    overview_file = mag_dir / 'overview.txt'
-    print(overview_file)
-    overview = pd.read_table(overview_file)
-    return overview
-
-
-def remove_HMMer_bounds(df):
-    # remove the start and stop position info from HMMER results
-    df['HMMER'] = df['HMMER'].str.split('(', expand=True)
-    df['HMMER'] = df['HMMER'].str.split('_', expand=True)
-    return df
-
-
-def decide_families(row):
-    # assign CAZyme families to a gene based on its dbCAN results
-    fams = [row['HMMER'], row['Hotpep'], row['DIAMOND']]
-
-    GH_count = 0
-    for i in fams:
-        if 'GH' in i:
-            GH_count += 1
-
-    tally = list(fams)
-    for c in fams:
-        if c == 'N':
-            tally.remove(c)
-        if '+' in c:
-            tally.remove(c)
-            tally = tally + c.split('+')
-                        
-    families = []
-    for f in tally:
-        if tally.count(f) > 1:
-            families.append(f)
-    families = set(families)
-
-    if not families and GH_count > 1:
-        families = set(['GH0']) # add GH0 for ambiguous GH family. 
-    if not len(families) == 0:
-        return families
-    
-
-def get_family_counts(overview_df, genome):
-    fam_counts = {}
-    for i, row in overview_df[genome].iterrows():
-        families = decide_families(row)
-        if families:
-            for fam in families:
-                if fam not in fam_counts.keys():
-                    fam_counts[fam] = 1
-                else:
-                    fam_counts[fam] += 1
-    return fam_counts
---
+++ src/data/make_dataset_helpers.py
@@ -0,0 +1,55 @@
+def get_mag_dbcan_overview(mag_dir):
+    # locate the overview file in the dbCAN results for a MAG
+    overview_file = mag_dir / 'overview.txt'
+    print(overview_file)
+    overview = pd.read_table(overview_file)
+    return overview
+
+
+def remove_HMMer_bounds(df):
+    # remove the start and stop position info from HMMER results
+    df['HMMER'] = df['HMMER'].str.split('(', expand=True)
+    df['HMMER'] = df['HMMER'].str.split('_', expand=True)
+    return df
+
+
+def decide_families(row):
+    # assign CAZyme families to a gene based on its dbCAN results
+    fams = [row['HMMER'], row['Hotpep'], row['DIAMOND']]
+
+    GH_count = 0
+    for i in fams:
+        if 'GH' in i:
+            GH_count += 1
+
+    tally = list(fams)
+    for c in fams:
+        if c == 'N':
+            tally.remove(c)
+        if '+' in c:
+            tally.remove(c)
+            tally = tally + c.split('+')
+                        
+    families = []
+    for f in tally:
+        if tally.count(f) > 1:
+            families.append(f)
+    families = set(families)
+
+    if not families and GH_count > 1:
+        families = set(['GH0']) # add GH0 for ambiguous GH family. 
+    if not len(families) == 0:
+        return families
+    
+
+def get_family_counts(overview_df, genome):
+    fam_counts = {}
+    for i, row in overview_df[genome].iterrows():
+        families = decide_families(row)
+        if families:
+            for fam in families:
+                if fam not in fam_counts.keys():
+                    fam_counts[fam] = 1
+                else:
+                    fam_counts[fam] += 1
+    return fam_counts
