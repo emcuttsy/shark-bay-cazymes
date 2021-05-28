c9aaaa0a618cdb307f23556a2bea5f9f09e1fafe
Elise <elise.m.cutts@gmail.com>
2021-05-28 10:22:04 -0400

    CAzyme_table

--- src/data/substrate_and_activity.py
+++ src/data/substrate_and_activity.py
@@ -24,7 +24,7 @@ def make():
     cazyfam_df = pd.read_table(data_dir / 'interim' / 'CAZyme_family_activities.tsv', sep='\t') #reimport introduces NaNs, which are needed.
     EC_df = pd.read_table(data_dir / 'raw' / 'final_manual_annos' / 'activities.tsv')
     sub_df = pd.read_table(data_dir / 'raw' / 'final_manual_annos' / 'substrates.tsv')
-    familes_df = pd.read_table(data_dir / 'processed' / 'CAZyme_ct_vs_MAG.tsv', index_col = 0)
+    families_df = pd.read_table(data_dir / 'processed' / 'CAZyme_ct_vs_MAG.tsv', index_col = 0)
     families_ex_df = pd.read_table(data_dir / 'processed' / 'CAZyme_ct_vs_MAG_ex.tsv', index_col = 0)
     
 
@@ -41,9 +41,15 @@ def make():
         else:
             return(0)
 
-    cazyfam_df['count'] = cazyfam_df['family'].apply(get_count_from_fam, args=(familes_df,))
+    cazyfam_df['count'] = cazyfam_df['family'].apply(get_count_from_fam, args=(families_df,))
     cazyfam_df['ex_count'] = cazyfam_df['family'].apply(get_count_from_fam, args=(families_ex_df,))
     cazyfam_df = cazyfam_df.iloc[: , 1:]
 
     cazyfam_df.to_csv(data_dir / 'interim' / 'CAZy_fams_autoannos_all.tsv', sep='\t')
     cazyfam_df[cazyfam_df['count'] > 0].to_csv(data_dir / 'interim' / 'CAZy_fams_autoannos_MAGs.tsv', sep='\t')
+
+    # make final tables based on manually refined file
+    cazyfam_df = pd.read_table(data_dir / 'raw' / 'final_manual_annos' / 'CAZy_fams_refined_MAGs.tsv')
+    cazyfam_df = helpers.add_substrate_metadata_cols(cazyfam_df, sub_df)
+    combined_cazyfam_df = helpers.make_combined_cazyfam_df(cazyfam_df, families_df, families_ex_df)
+    combined_cazyfam_df.to_csv(data_dir / 'processed' / 'CAZyme_table.tsv', sep='\t')
